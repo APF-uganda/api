ed75f71e0f0ff1397f6080e51f8c5a0e28e453fa
Nowellah <abridgetnowellah@gmail.com>
2026-05-05 13:46:12 +0300

    add a package

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,9 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
+# Import is optional - only loads if Celery is installed
+try:
+    from .celery import app as celery_app
+    __all__ = ('celery_app',)
+except ImportError:
+    # Celery not installed - that's okay, you can use cron instead
+    pass
